07822ed220885c3c17cc2b7d2113873875f4968f
vlodko-afk <63566106+vlodko-afk@users.noreply.github.com>
2020-04-13 03:02:25 +0300

    Delete food.py

--- food.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import random
-class Food:
-    def __init__(self):
-        self.food_position = []
-    def get_food_position(self, qui):
-        self.food_position = random.choice(qui.field)
-    def draw_food(self,win):
-        pygame.draw.rect(win,pygame.Color("Red"),pygame.Rect(self.food_position[0],self.food_position[1],10,10))
